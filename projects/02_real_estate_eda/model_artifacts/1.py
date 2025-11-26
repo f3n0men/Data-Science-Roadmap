43b2b3d1aa0578a9b21a8da472c89d6afef58eb2
f3n0men <volodya.vvu@gmail.com>
2025-11-26 22:53:13 +0300

    Add real estate price prediction web app from model_artifacts folder

---
+++ projects/02_real_estate_eda/model_artifacts/1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e47b913797494f5a4258b724a64eb95eeae63436a693ba04d6bc8cf4a96df78e
+size 953
--- projects/02_real_estate_eda/model_artifacts/prediction_function.py
+++ projects/02_real_estate_eda/model_artifacts/prediction_function.py
@@ -1,28 +1,3 @@
-
-def predict_apartment_price(area, rooms, minutes_to_metro, floor, total_floors, metro_station=None):
-    """
-    Предсказывает цену квартиры в Москве
-    """
-    import pandas as pd
-    import joblib
-
-    # Загружаем модель и данные
-    model = joblib.load('apartment_price_predictor.pkl')
-
-    features = {
-        'Area': area,
-        'Number of rooms': rooms,
-        'Minutes to metro': minutes_to_metro,
-        'Floor': floor,
-        'Number of floors': total_floors,
-        'is_premium_station': 0,
-        'is_budget_station': 0
-    }
-
-    if metro_station in premium_stations:
-        features['is_premium_station'] = 1
-    elif metro_station in budget_stations:
-        features['is_budget_station'] = 1
-
-    input_df = pd.DataFrame([features])
-    return model.predict(input_df)[0]
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e08c4f50e03084df8912d90406c9137fe8182c627df60e79f5bf8aa9eaf2ff5
+size 1277
